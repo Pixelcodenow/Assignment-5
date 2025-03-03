eb69bd8d943087142b48fe8b0342202feaa3ad1a
pixelcodenow <pixelcodenow@gmail.com>
2025-03-03 17:52:09 +0600

    push problem

--- backpage.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("backButton").addEventListener("click", function () {
-        history.back();
-    });
-});
--- newPage.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("clickableDiv").addEventListener("click", function () {
-        window.location.href = "test2.html";
-    });
-});
